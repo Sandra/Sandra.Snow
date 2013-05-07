32caf61b96839a7c5e75271237259ea1bd6d9481
Jonathan Channon <jonathan.channon@gmail.com>
2013-05-07 21:27:15 +0100

    Setup Github/World Domination I think and removed LibGit2Sharp

---
+++ src/Sandra.Snow.Barbato/AuthenticationProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Authentication.WorldDomination;
+using WorldDomination.Web.Authentication;
+
+namespace Sandra.Snow.Barbato
+{
+    public class AuthenticationProvider : IAuthenticationCallbackProvider
+    {
+        public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
+        {
+            return nancyModule.Negotiate.WithView("AuthenticateCallback").WithModel(model);
+        }
+
+        public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
