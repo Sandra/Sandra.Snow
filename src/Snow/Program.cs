fd90942aa0d34c56a6aea5de26ff98471b52d1ae
Phillip Haydon <phillip.haydon@gmail.com>
2013-12-25 02:42:14 +0800

    added new server option (awesome stuff)

--- src/Snow/Program.cs
+++ src/Snow/Program.cs
@@ -1,14 +1,19 @@
 ﻿namespace Snow
 {
+    using System.Diagnostics;
     using Enums;
     using Exceptions;
     using Extensions;
+    using Microsoft.Owin.FileSystems;
+    using Microsoft.Owin.Hosting;
+    using Microsoft.Owin.StaticFiles;
     using Models;
     using Nancy;
     using Nancy.Testing;
     using Nancy.ViewEngines.Razor;
     using Nancy.ViewEngines.SuperSimpleViewEngine;
     using Newtonsoft.Json;
+    using Owin;
     using StaticFileProcessors;
     using System;
     using System.Collections.Generic;
@@ -44,7 +49,7 @@
                     currentDir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
                 }
 
-                currentDir.OutputIfDebug(prefixWith: "current directory: ");
+                currentDir.OutputIfDebug(prefixWith: " - Current directory: ");
 
                 var settings = CreateSettings(currentDir);
 
@@ -125,8 +130,11 @@
 
                 Console.WriteLine("Sandra.Snow : " + DateTime.Now.ToString("HH:mm:ss") + " : Finish processing");
 
-                new SnowServer().Start(settings);
-                
+                //if (commands.ContainsKey("server"))
+                {
+                    SnowServer.Start(settings);
+                }
+
                 if (commands.ContainsKey("debug"))
                 {
                     DebugHelperExtensions.WaitForContinue();
@@ -135,6 +143,7 @@
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString());
 
                 DebugHelperExtensions.WaitForContinue();
             }
@@ -175,9 +184,9 @@
                 Console.WriteLine();
                 Console.WriteLine("Error processing static file: ");
                 Console.WriteLine();
-                Console.WriteLine("- Loop: " + staticFile.Loop);
-                Console.WriteLine("- File: " + staticFile.File);
-                Console.WriteLine("- Message:");
+                Console.WriteLine(" - Loop: " + staticFile.Loop);
+                Console.WriteLine(" - File: " + staticFile.File);
+                Console.WriteLine(" - Message:");
                 Console.WriteLine();
                 Console.WriteLine(exception.Message);
 
--- src/Snow/SnowServer.cs
+++ src/Snow/SnowServer.cs
@@ -1,53 +1,36 @@
 ﻿namespace Snow
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
-    using System.Net;
-    using System.Text;
-    using Extensions;
+    using Microsoft.Owin.FileSystems;
+    using Microsoft.Owin.Hosting;
+    using Microsoft.Owin.StaticFiles;
+    using Owin;
 
     public class SnowServer
     {
-        public void Start(SnowSettings settings)
+        public static void Start(SnowSettings settings)
         {
-            Directory.SetCurrentDirectory(settings.Output);
-
-            using (var server = new HttpListener())
+            Console.WriteLine("Sandra.Snow : Starting up a testing server");
+            
+            var fileSystem = new FileServerOptions
             {
-                server.Prefixes.Add("http://localhost:1234/");
-                server.Start();
+                EnableDirectoryBrowsing = true,
+                FileSystem = new PhysicalFileSystem(Path.GetFullPath(settings.Output))
+            };
 
-                "Listening on http://localhost:1234/".OutputIfDebug();
+            using (WebApp.Start("http://*:1234/", app => app.UseFileServer(fileSystem)))
+            {
+                Console.WriteLine("Sandra.Snow : Listening on http://locahost:1234/");
+                Console.WriteLine(" - attempting to open your browser...");
+                
+                Process.Start("http://localhost:1234/");
 
-                while (true)
-                {
-                    var context = server.GetContext();
-                    var response = context.Response;
-
-                    var requestUrl = Directory.GetCurrentDirectory() + context.Request.Url.LocalPath;
-
-                    if (context.Request.Url.LocalPath == "/")
-                    {
-                        requestUrl = Directory.GetCurrentDirectory() + "/index.html";
-                    }
-
-                    using (var tr = new FileStream(requestUrl, FileMode.Open))
-                    using (var output = response.OutputStream)
-                    {
-                       tr.CopyTo(output);
-                        output.Flush();
-                        //var msg = tr.ReadToEnd();
-
-                        //var buffer = Encoding.UTF8.GetBytes(msg);
-
-                        //response.ContentLength64 = buffer.Length;
-                        //var st = response.OutputStream;
-                        //st.Write(buffer, 0, buffer.Length);
-                        //st.Flush();
-                    }
-
-                    context.Response.Close();  // here we close the connection
-                }
+                Console.WriteLine(" - press any to quit the testing server...");
+                Console.ReadKey();
+                Console.WriteLine("");
+                Console.WriteLine("Sandra.Snow : Exited testing server");
             }
         }
     }
