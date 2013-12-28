7813bdb11225b592d2845bc43f858bfe9f07c8c5
Henrik Andersson <henrik.a.andersson@outlook.com>
2013-12-28 15:29:29 +1000

    Context aware keywords
    On main page, keywords will contain all categories from all posts.
    On post page, keywords will contain all categories from the post
    currently being viewed.

--- src/Snow/ViewModels/BaseViewModel.cs
+++ src/Snow/ViewModels/BaseViewModel.cs
@@ -9,6 +9,7 @@ namespace Snow.ViewModels
         public string GeneratedDate { get; set; }
         public List<MonthYear> MonthYearList { get; set; }
         public List<Category> AllCategories { get; set; }
+        public List<string> Keywords { get; set; }
         public Category Category { get; set; }
         public string GeneratedUrl { get; set; }
         public Published Published { get; set; }
