9643cc33f48be91dbf42ec5a94b7aa59bbb4f591
Johan Nilsson <jonh04@gmail.com>
2014-05-01 17:37:11 -0400

    Do not show posts published for the future

--- src/Snow/ShouldProcess.cs
+++ src/Snow/ShouldProcess.cs
@@ -2,17 +2,18 @@
 {
     using Enums;
     using Models;
+    using System;
 
     public static class ShouldProcess
     {
         public static bool Archive(Post post)
         {
-            return IsPublished(post);
+            return IsPublished(post) && IsInThePast(post);
         }
 
         public static bool Category(Post post)
         {
-            return IsPublished(post);
+            return IsPublished(post) && IsInThePast(post);
         }
 
         public static bool Category(Category category)
@@ -22,12 +23,17 @@
 
         public static bool Feed(Post post)
         {
-            return IsPublished(post);
+            return IsPublished(post) && IsInThePast(post);
         }
 
         public static bool Posts(Post post)
         {
-            return IsPublished(post);
+            return IsPublished(post) && IsInThePast(post);
+        }
+
+        private static bool IsInThePast(Post post)
+        {
+            return post.Date < DateTime.Now;
         }
 
         private static bool IsPublished(Post post)
